aa8439c828d955869932accc0b810f21a8dd6142
nouckelson <jnouckelson@hotmail.com>
2018-02-26 19:34:18 -0400

    Update TwitterClient.java

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -17,8 +17,8 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
 	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1/"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "4PTNlxvKo4qccJPWUG0bB8xGc";
-	public static final String REST_CONSUMER_SECRET = "WN8HZDXfn1C3WAZ5J5zAqtTvVtoG8DM4dZqMi5QX3MIlQ9jXYW";
+	public static final String REST_CONSUMER_KEY = "You key";
+	public static final String REST_CONSUMER_SECRET = "your secret key";
 	public static final String REST_CALLBACK_URL = "oauth://cpsimpletweets";
 	//public static final String REST_CALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
 
